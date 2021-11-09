413d2833d4b441e5bd283e5881d90a9b7b0133a6
radioaktivity <41319561+radioaktivity@users.noreply.github.com>
2021-11-09 09:08:38 +0100

    Extrapolation to surfaces

--- main.py
+++ main.py
@@ -23,21 +23,35 @@ def get_tangent(x,y,dxdy, width=0.1):
 
 def main():
     atm.gamma
+    t = 0
+    t_end = 100
+    nx = 3
+    ny = 2
 
     start = timeit.default_timer()
-    [cells, points] = create_mesh(nx=3,ny=2, plot_cells=True)
+    [cells, points] = create_mesh(nx=nx,ny=ny, plot_cells=True)
     print(f"Mesh Runtime : {timeit.default_timer()-start}")
 
     xplot = []
     rhoplot = []
     tangents = []
+    faces = set()
     for c in cells:
         print(c)
         print('-'*50)
         [rho_dx, rho_dy, u_dx, u_dy, v_dx, v_dy, p_dx, p_dy] = c.calc_gradients_weighted_sum()
+        c.extrapol2faces()
         xplot.append(c.center.X)
         rhoplot.append(c.rho)
-        tangents.append(get_tangent(c.center.X, c.rho, rho_dx))
+        tangents.append(get_tangent(c.center.X, c.rho, rho_dx, width=1/(3*nx)))
+        for f in c.faces:
+            faces.add(f)
+
+    for f in faces:
+        plt.scatter(f.center.X, f.rho_L, color=['green'], marker='^')
+        plt.scatter(f.center.X, f.rho_R, color=['red'], marker='^')
+
+
 
     print("-"*50)
     plt.scatter(xplot,rhoplot)
